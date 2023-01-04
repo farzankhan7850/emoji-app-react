824ba412fc0e2c5f61caa29611397f629d8fa8fe
Farzan Khan <farzankhan7850@gmail.com>
2023-01-04 20:25:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+	import React, { useState } from "react";
 import './App.css';
+// import Header from './components/Header';
+
+
+const emojiDictionary = {
+  "😜": 'Naughty',
+  "😂": 'Laughing',
+  "😋": 'Tasty',
+  "🤒": 'Fever',
+  "😴": 'Sleeping',
+  "😡": 'Angry'
+};
+
+
 
 function App() {
+
+
+  const [emojiSearch, setEmoji] = useState('');
+
+                        const emojiHandler = (event) => {
+
+                          let val = event.target.value;
+                          let meaning = emojiDictionary[val];
+
+                          if (meaning === undefined)
+                            meaning = "We don't have this in our database";
+
+                          setEmoji(meaning);
+                        }
+
+  let emojisWeHave = Object.keys(emojiDictionary);
+
+  const emojiClicked = (emoji) =>{
+    let meaning = emojiDictionary[emoji];
+    setEmoji(meaning);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center' }} className='app' >
+      <h1 >inside outtt!</h1>
+      <input onChange={emojiHandler} placeholder="Enter emoji here" ></input>
+      <h4> {emojiSearch}</h4>
+
+
+      <h3>Emojis we know</h3> 
+      {
+        emojisWeHave.map((e) => {
+          return <span onClick={ ()=> emojiClicked(e)}
+           key={e} 
+           style={{ fontSize: '2rem', padding: '0.7rem',
+            cursor: 'pointer' }}
+            >{e}</span>
+        })
+      }
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+function Header(){
+
+    let heading = 'Making emoji selector with the mentoring of neoGcamp';
+
+    return <h2>{ heading }</h2>
+}
+
+export default Header;
